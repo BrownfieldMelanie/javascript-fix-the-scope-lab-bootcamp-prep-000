5cb444f426e0a63e7239e5db64ff7be07b05f1e0
Melanie Brownfield <melanie.brownfield1@gmail.com>
2018-04-01 19:29:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,19 +11,21 @@ function yourAnimal() {
   return animal
 }
 
+
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
-  const two = 2
+  return n + 2
 }
-
 var funkyFunction = function() {
   return function() {
+    
     return "FUNKY!"
+    
   }
 }
 
+
+
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+// NOTE: you only need to modify the code below this lin
+
+
